f8dab6c4935ab8849f0fea050a15d4fb49fb5e01
Bienvenido Velez <Bienvenido.Velez@upr.edu>
2021-11-21 16:36:01 -0400

    Added selectionSort, insertionSort and mergeSort

--- SearchingAndSorting.cpp
+++ SearchingAndSorting.cpp
@@ -36,6 +36,92 @@ int binarySearch(vector<int> numbers, int key) {
 }
 
 
+void selectionSort(vector<int> &numbers) {
+
+    for (int i=0; i<numbers.size()-1; i++) {
+
+        int minPos = i;
+        for (int j=i; j<numbers.size(); j++) {
+            if (numbers[j] < numbers[minPos]) {
+                minPos = j;
+            }
+        }
+        int temp = numbers[i];
+        numbers[i] = numbers[minPos];
+        numbers[minPos] = temp;
+    }
+
+}
+
+void insertionSort(vector<int> &numbers) {
+
+    for (int i=1; i<numbers.size(); i++) {
+
+        int j = i;
+        while (j > 0 && numbers[j] < numbers[j-1]) {
+            int temp = numbers[j];
+            numbers[j] = numbers[j-1];
+            numbers[j-1] = temp;
+            j--;
+        }
+
+    }
+
+}
+
+void merge(vector<int> left, vector<int> right, vector<int> &result) {
+
+    int i = 0;
+    int j = 0;
+    int k = 0;
+
+    while (i<left.size() && j<right.size()) {
+        if (left[i] < right[j]) {
+            result[k] = left[i];
+            i++;
+        } else {
+            result[k] = right[j];
+            j++;
+        }
+        k++;
+    }
+
+    while (i < left.size()) {
+        result[k] = left[i];
+        i++;
+        k++;
+    }
+
+    while (j < right.size()) {
+        result[k] = right[j];
+        j++;
+        k++;
+    }
+
+}
+
+void mergeSort(vector<int> &numbers) {
+
+    if (numbers.size() <= 1) return;
+
+    int mid = numbers.size() / 2;
+    vector<int> leftHalf;
+    for (int i=0; i<mid; i++) {
+        leftHalf.push_back(numbers[i]);
+    }
+    vector<int> rightHalf;
+    for (int i=mid; i<numbers.size(); i++) {
+        rightHalf.push_back(numbers[i]);
+    }
+
+    mergeSort(leftHalf);
+    mergeSort(rightHalf);
+    merge(leftHalf, rightHalf, numbers);
+
+
+}
+
+
 
 
 int main() {
@@ -50,5 +136,11 @@ int main() {
     cout << "Position of 5 is: " << binarySearch(myNumbers,5) << endl;
     cout << "Position of 7 is: " << binarySearch(myNumbers,7) << endl;
     cout << "Position of 8 is: " << binarySearch(myNumbers,8) << endl;
+
+    vector<int> unsortedNumbers { 8, 7, 6, 5, 4, 3, 2, 1, 0 };
+    mergeSort(unsortedNumbers);
+
+    cout << "End" << endl;
+
 
 }
